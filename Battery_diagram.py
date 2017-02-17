2ab7cce34f46097f6e2701eef5bef21dc07da585
Behnam Tonekaboni <behnam.tfn@gmail.com>
2017-02-18 07:45:08 +1000

    j and k labels added to axes

--- Battery_diagram.py
+++ Battery_diagram.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.figure(1)
+plt.figure(1,figsize=[10,8])
 plt.clf()
 
-jmax=5
+jmax=10
 ax = plt.gca()
 # Ploting the levels
 for j in range(jmax):
@@ -18,10 +18,21 @@ ax.spines['bottom'].set_lw(2)
 
 ymin,ymax = ax.get_ylim()
 ax.set_ylim([-0.5,ymax])
+ax.set_xticks(range(jmax))
+ax.set_yticks(range(jmax))
 
-# Annotating
+ax.set_xlabel(r'$k$', fontsize=20, x=1,y=1)
+ax.set_ylabel(r'$j$', fontsize=20, x=1,y=1)
+
+#%%
+# Annotating for states
 for j in range(jmax):
     for k in range(j+1):
-        ax.text(-0.2+k,0.1+j,r'$|'+str(j)+','+str(k)+r'\rangle$')
-        ax.text(-0.2+k,-0.2+j,r'$|'+str(j-k)+','+str(k)+r'\rangle$',color='C0')
+        ax.text(-0.2+k,0.1+j,r'$|'+str(j)+','+str(k)+r'\rangle$') # |j,k>
+        ax.text(-0.2+k,-0.2+j,r'$|'+str(j-k)+','+str(k)+r'\rangle$',color='C0') #|l,k>
+
+#%%
+# Transitions
+
+
 plt.show()
